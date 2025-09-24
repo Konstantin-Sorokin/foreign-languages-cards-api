0d393ad107551f3217702fa5b608858f4ec5151f
konstantin sorokin <konstantin.sorokin.work@gmail.com>
2025-09-24 22:03:46 +0300

    refactor: change type field from str to CardType(StrEnum)

--- src/app/models/pack.py
+++ src/app/models/pack.py
@@ -3,10 +3,11 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 from app.models.base import Base
 from app.models.mixins import IdPkMixin
+from app.utils.card_types import CardType
 
 
 class Pack(IdPkMixin, Base):
 
     name: Mapped[str] = mapped_column(String(30))
     description: Mapped[str] = mapped_column(String(255))
-    card_type: Mapped[str] = mapped_column(String(30))
+    card_type: Mapped[CardType]
