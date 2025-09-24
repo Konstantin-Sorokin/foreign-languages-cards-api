cc60c729ed312edab6a9d65f3f90bb6c9d797732
konstantin sorokin <konstantin.sorokin.work@gmail.com>
2025-09-24 15:32:16 +0300

    refactor: make changes to the methods and remove unnecessary methods

--- src/app/services/card.py
+++ src/app/services/card.py
@@ -1,44 +1,29 @@
-from typing import Annotated
-
 from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi import Depends
 
-from app.models.card import TranslationCard, IrregularVerbCard
+from app.models import TranslationCard, IrregularVerbCard
 from app.services.base import BaseService
-from app.schemas.card import (
-    TranslationCardCreate,
-    IrregularVerbCardCreate,
-)
-from app.schemas.progress import ProgressRead
-from app.utils import db_helper
 
 
 class CardService(BaseService):
 
-    async def create_translation_card(
-        self, data: TranslationCardCreate
-    ) -> TranslationCard:
-        card = TranslationCard(**data.model_dump())
-        self.session.add(card)
-        await self.session.commit()
-        return card
-
     async def create_irregular_verb_card(
-        self, data: IrregularVerbCardCreate
+        self,
+        base_form: str,
+        past_simple: str,
+        past_participle: str,
+        translation: str,
+        pack_id: int,
     ) -> IrregularVerbCard:
-        card = IrregularVerbCard(**data.model_dump())
+        card = IrregularVerbCard(
+            base_form=base_form,
+            past_simple=past_simple,
+            past_participle=past_participle,
+            translation=translation,
+            pack_id=pack_id,
+        )
         self.session.add(card)
         await self.session.commit()
         return card
-
-    async def get_translation_card_by_id(self, card_id: int) -> TranslationCard | None:
-        return await self.session.get(TranslationCard, card_id)
-
-    async def get_irregular_verb_card_by_id(
-        self, card_id: int
-    ) -> IrregularVerbCard | None:
-        return await self.session.get(IrregularVerbCard, card_id)
 
     async def get_irregular_verb_cards_by_pack_id(
         self, pack_id: int
@@ -47,25 +32,18 @@ class CardService(BaseService):
         cards = await self.session.scalars(stmt)
         return list(cards)
 
-    async def add_card_to_progress(
-        self, user_id: int, data: TranslationCardCreate
-    ) -> ProgressRead:
-
+    async def get_or_create_card(
+        self, original: str, translation: str
+    ) -> TranslationCard:
         stmt = select(TranslationCard).where(
-            TranslationCard.original == data.original,
-            TranslationCard.translated == data.translated,
+            TranslationCard.original == original,
+            TranslationCard.translation == translation,
         )
-
         card = await self.session.scalar(stmt)
-
         if card:
-            ...
+            return card
 
-
-def get_card_service(
-    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-) -> CardService:
-    """
-    Возвращает экземпляр CardService с переданной сессией.
-    """
-    return CardService(session=session)
+        card = TranslationCard(original=original, translation=translation)
+        self.session.add(card)
+        await self.session.flush()
+        return card
