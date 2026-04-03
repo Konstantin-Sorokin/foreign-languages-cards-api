71f0211bb23d106602cdbced5f1251712d090a2b
konstantin <konstantin.sorokin.work@gmail.com>
2026-04-03 19:00:10 +0300

    Add: timezone=True in field next_repeat by Progress model

--- src/app/models/progress.py
+++ src/app/models/progress.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import TYPE_CHECKING
 
-from sqlalchemy import ForeignKey, Integer
+from sqlalchemy import DateTime, ForeignKey, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.models.base import Base
@@ -11,13 +11,11 @@ if TYPE_CHECKING:
 
 
 class Progress(Base):
-    user_id: Mapped[int] = mapped_column(
-        ForeignKey("users.telegram_id"), primary_key=True
-    )
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
     card_id: Mapped[int] = mapped_column(
         ForeignKey("translation_cards.id"), primary_key=True
     )
     interval_level: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
-    next_repeat: Mapped[datetime]
+    next_repeat: Mapped[datetime] = mapped_column(DateTime(timezone=True))
 
     card: Mapped["TranslationCard"] = relationship("TranslationCard")
