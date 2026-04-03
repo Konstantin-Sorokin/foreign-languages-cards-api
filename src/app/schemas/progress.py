84ee6b0e43912849212c696587e903c8d423b943
konstantin <konstantin.sorokin.work@gmail.com>
2026-04-03 18:59:01 +0300

    Delete: ProgressRead scheme.

--- src/app/schemas/progress.py
+++ src/app/schemas/progress.py
@@ -1,12 +1,5 @@
 from pydantic import BaseModel
 
-from app.schemas.card import TranslationCardRead
-
-
-class ProgressRead(BaseModel):
-    interval: int
-    card: TranslationCardRead
-
 
 class ProgressUpdate(BaseModel):
     success: bool
