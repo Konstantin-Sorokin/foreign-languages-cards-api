e793f30c028135e9566252d5442bead5d304fcbb
konstantin <konstantin.sorokin.work@gmail.com>
2026-05-17 18:13:00 +0300

    Add: health check handler

--- src/app/main.py
+++ src/app/main.py
@@ -7,6 +7,12 @@ from app.utils import settings
 app = FastAPI()
 app.include_router(router)
 
+
+@app.get("/health", include_in_schema=False)
+async def health():
+    return {"status": "ok"}
+
+
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
